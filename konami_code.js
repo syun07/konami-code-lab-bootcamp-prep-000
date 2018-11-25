ca7ad1683763f267680d25286040685cdb2c7e90
Sarah Yun <sarahyun7@gmail.com>
2018-11-25 02:20:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,23 +11,57 @@ const codes = [
   "a"
 ];
 
+
+var index = 0;
+
 function init() {
-  let index = 0;
-
-  document.body.addEventListener('keydown', onKeyDownHandler);
-
-  function onKeyDownHandler(e) {
+  
+  var body = document.body;
+  
+  body.addEventListener('keydown', function(e) {
     const key = e.key;
-
-    if (codes[index] == key) {
-      index++;
-      if (index === codes.length) {
-        alert('Hurray!');
-
-        index = 0;
-      }
-    } else {
-      index = 0;
+    
     }
   }
 }
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+//   let index = 0;
+
+//   document.body.addEventListener('keydown', onKeyDownHandler);
+
+//   function onKeyDownHandler(e) {
+//     const key = e.key;
+
+//     if (codes[i] == key) {
+//       i++;
+//       if (i === codes.length) {
+//         alert('Hurray!');
+
+//         i = 0;
+//       }
+//     } else {
+//       i = 0;
+//     }
+//   }
+// }
+
+// init();
